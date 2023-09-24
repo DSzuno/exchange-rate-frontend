92b493db74833a806483b04517365321a7815cd3
Daniel Szunomar <daniel.szunomar@gmail.com>
2023-09-24 13:00:23 +0100

    feat: add functionality and component usage to root exchange rate component

--- src/components/exchange-rate/exchange-rate.js
+++ src/components/exchange-rate/exchange-rate.js
@@ -1,9 +1,96 @@
-import {Card, CardContent} from "@mui/material";
+import { Card } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import { fetchExchangeRare } from "../../api/exchange-rate.api";
+import { ExchangeRateHeader } from "./exchange-rate-header";
+import { ExchangeRateList } from "./exchange-rate-list";
+import { ExchangeRateConfirmationDialog } from "./exchange-rate-confirmation-dialog";
 
 export const ExchangeRate = () => {
-    return (
-        <Card>
-            Header
-            <CardContent>Content</CardContent>
-        </Card>)
-}
+  const updateFrequency = process.env.UPDATE_FREQUNCY || 180000;
+  const timerIdRef = useRef(null);
+  const [selectedSymbol, setSelectedSymbol] = useState("EUR");
+  const [rateList, setRateList] = useState([]);
+  const [isListLoading, setIsListLoading] = useState(false);
+  const [lastUpdate, setLastUpdate] = useState(null);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [selectedSymbolToChange, setSelectedSymbolToChange] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    loadRates();
+  }, [selectedSymbol]);
+
+  useEffect(() => {
+    const pollingCallback = () => {
+      console.log("Polling...");
+      loadRates();
+    };
+
+    const startPolling = () => {
+      timerIdRef.current = setInterval(pollingCallback, updateFrequency);
+    };
+
+    const stopPolling = () => {
+      clearInterval(timerIdRef.current);
+    };
+
+    startPolling();
+
+    return () => {
+      stopPolling();
+    };
+  }, [rateList]);
+
+  const loadRates = () => {
+    setIsListLoading(true);
+    setError(null);
+    fetchExchangeRare(selectedSymbol)
+      .then((rateList) => {
+        setRateList(Object.entries(rateList.rate));
+        setIsListLoading(false);
+        setLastUpdate(new Date());
+      })
+      .catch((error) => {
+        setIsListLoading(false);
+        setError(error?.message || true);
+      });
+  };
+
+  const handleRateListChangeRequest = (symbol) => {
+    setSelectedSymbolToChange(symbol);
+    setIsDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
+    setSelectedSymbolToChange(null);
+  };
+
+  const handleDialogAgree = () => {
+    setSelectedSymbol(selectedSymbolToChange);
+    handleDialogClose();
+  };
+
+  return (
+    <Card>
+      <ExchangeRateHeader
+        baseSymbol={selectedSymbol}
+        lastUpdate={lastUpdate}
+        updateFrequency={updateFrequency}
+      />
+      <ExchangeRateList
+        baseSymbol={selectedSymbol}
+        isLoading={isListLoading}
+        rateList={rateList}
+        onChangeBaseSymbol={handleRateListChangeRequest}
+        error={error}
+      />
+      <ExchangeRateConfirmationDialog
+        isOpen={isDialogOpen}
+        symbol={selectedSymbolToChange}
+        onAgree={handleDialogAgree}
+        onClose={handleDialogClose}
+      />
+    </Card>
+  );
+};
