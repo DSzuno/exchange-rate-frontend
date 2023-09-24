a44fff7a1e7436dfe062adfe8fa51e875a7d2f2e
Daniel Szunomar <daniel.szunomar@gmail.com>
2023-09-24 12:30:44 +0100

    feat: add empty list card component

---
+++ src/components/layout/empty-list-card.js
@@ -0,0 +1,18 @@
+import {Box, CardContent, Container, Divider, Typography} from "@mui/material";
+import {SearchOff} from "@mui/icons-material";
+
+export const EmptyListCard = ({icon, message}) => {
+    return (<Container sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <Box>
+            <CardContent sx={{textAlign: 'center'}}>
+                {icon || <SearchOff sx={{fontSize: 100}} color="primary"/>}
+            </CardContent>
+            <Divider variant="middle"/>
+            <CardContent>
+                <Typography variant="h5" align="center" color="text.secondary">
+                    {message || "No search result"}
+                </Typography>
+            </CardContent>
+        </Box>
+    </Container>);
+}
